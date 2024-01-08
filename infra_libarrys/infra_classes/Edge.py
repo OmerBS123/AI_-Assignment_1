85fa7db3c9da2c1ed37cae3eef002064a1272e35
Omer Ben Salmon <o_bensalmon@apple.com>
2024-01-08 19:19:14 +0200

    added djkstra class

--- infra_libarrys/infra_classes/Edge.py
+++ infra_libarrys/infra_classes/Edge.py
@@ -3,13 +3,12 @@ from infra_libarrys.consts_and_enums.EdgeConsts import DEFAULT_WEIGHT
 
 class Edge:
     def __init__(self, nodes=None, is_fragile=False, weight=DEFAULT_WEIGHT):
-        # 'regular', 'fragile', 'blocked'
-        self.nodes = nodes  # List of connected nodes
+        self.nodes = nodes
         self.is_fragile = is_fragile
         self.weight = weight
 
     def add_nodes(self, node1, node2):
         self.nodes = {node1, node2}
 
-    def __str__(self):
-        return f'Edge(State: {self.state}, Connected Nodes: {self.nodes})'
+    def get_neighbor_node(self, curr_node):
+        return (self.nodes - {curr_node}).pop()
