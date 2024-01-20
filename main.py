17fb876f281b1618de49b13aa7b13c3d157057b8
Omer Ben Salmon <o_bensalmon@apple.com>
2024-01-20 22:57:46 +0200

    reverted back to main

--- main.py
+++ main.py
@@ -6,25 +6,17 @@ from infra_libarrys.infra_classes.Flow import Flow
 from infra_libarrys.infra_classes.Gui import GraphUI
 
 
-# def main():
-#     config_path = "/Users/omerbensalmon/Desktop/BGU/Semester_5/inroduction_to_AI/Home_Assigments/Assigment_1/support_files/config.txt"
-#     parser_dict = parse_args(config_path)
-#     env, agents_list, package_appear_dict, package_disappear_dict = get_flow_args(parser_dict)
-#     gui = GraphUI(env, agents_list)
-#     flow = Flow(env, agents_list, package_appear_dict, package_disappear_dict, gui_handler=gui)
-#     gui.set_flow(flow)
-#     flow_thread = threading.Thread(target=flow.run_flow)
-#     flow_thread.start()
-#     gui.run_ui()
+def main():
+    config_path = "/Users/omerbensalmon/Desktop/BGU/Semester_5/inroduction_to_AI/Home_Assigments/Assigment_1/support_files/config.txt"
+    parser_dict = parse_args(config_path)
+    env, agents_list, package_appear_dict, package_disappear_dict = get_flow_args(parser_dict)
+    gui = GraphUI(env, agents_list)
+    flow = Flow(env, agents_list, package_appear_dict, package_disappear_dict, gui_handler=gui)
+    gui.set_flow(flow)
+    flow_thread = threading.Thread(target=flow.run_flow)
+    flow_thread.start()
+    gui.run_ui()
 
-
-def a():
-    return 1,2
 
 if __name__ == "__main__":
-    res = a()
-    # main()
-    print(res)
-
-
-
+    main()
